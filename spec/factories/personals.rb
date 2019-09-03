376aabaa3e7973e88fcdf080ee450f595d900dd7
Daniela e Leticia <treinadev+par04@campuscode.com.br>
2019-09-03 16:57:12 -0300

    WIP Personal registra agenda
    Co-authored-by: Daniela <dani.adamatti@gmail.com>
    Co-authored-by: Leticia <hashizumemay@gmail.com>

--- spec/factories/personals.rb
+++ spec/factories/personals.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :personal do
     work_document { "MyString" }
+    email { "teste@email.com" }
+    password { "123456" }
   end
 end
--- spec/features/personal_creates_schedule_spec.rb
+++ spec/features/personal_creates_schedule_spec.rb
@@ -2,14 +2,34 @@ require 'rails_helper'
 
 feature 'Personal creates schedule' do
   scenario 'and they should be logged in' do
-    personal = Personal.create(email: 'personal@email.com', password: '123456')
+    personal = create(:personal)
 
     visit root_path
     click_on 'Entrar'
-    fill_in 'Email', with: 'personal@email.com'
+    fill_in 'Email', with: 'teste@email.com'
     fill_in 'Senha', with: '123456'
     click_on 'Log in'
 
     expect(page).to have_content('Cadastrar agenda')
+  end
+
+  scenario 'successfully' do
+    personal = create(:personal)
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'Email', with: 'teste@email.com'
+    fill_in 'Senha', with: '123456'
+    click_on 'Log in'
+    click_on 'Cadastrar agenda'
+    check('Segunda-feira')
+    check('Quarta-feira')
+    fill_in 'Inicio', with: '8'
+    fill_in 'Termino', with: '16'
+    fill_in 'Valor', with: '35'
+    click_on 'Salvar'
+
+    expect(page).to have_content('Cadastrado com sucesso')
+
   end
 end
