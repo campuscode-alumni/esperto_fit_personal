21d6032b7fd527b964acadbf5c83badeab33b8c0
Daniela e Leticia <treinadev+par04@campuscode.com.br>
2019-09-05 15:24:52 -0300

    Personal se autentica para criar agenda
    Co-authored-by: Leticia <hashizumemay@gmail.com>
    Co-authored-by: Daniela <dani.adamatti@gmail.com>

--- spec/features/personal_authenticates_to_create_schedule_spec.rb
+++ spec/features/personal_authenticates_to_create_schedule_spec.rb
@@ -10,13 +10,17 @@ feature 'Personal authenticates to create schedule' do
     fill_in 'Senha', with: '123456'
     click_on 'Log in'
 
-    expect(page).to have_content('Cadastrar agenda')
+    expect(page).to have_link('Cadastrar agenda')
+    expect(page).to have_link('Sair')
+
   end
 
   scenario 'and a not loged in personal cant create schedule' do
     visit root_path
 
-    expect(page).not_to have_content('Cadastrar agenda')
+    expect(page).not_to have_link('Cadastrar agenda')
+    expect(page).to have_link('Entrar')
+
   end
 
   scenario 'and customers cant create schedule' do
@@ -28,6 +32,19 @@ feature 'Personal authenticates to create schedule' do
     fill_in 'Senha', with: '123456'
     click_on 'Log in'
 
-    expect(page).not_to have_content('Cadastrar agenda')
+    expect(page).not_to have_link('Cadastrar agenda')
+  end
+
+  scenario 'and signed in user can sign out' do
+    customer = create(:account, email: 'teste@email.com', password: '123456')
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'Email', with: 'teste@email.com'
+    fill_in 'Senha', with: '123456'
+    click_on 'Log in'
+    click_on 'Sair'
+
+    expect(page).to have_link('Novo Usuario')
   end
 end
