25455a1e4b050a0828832919385a595dc2618ca1
Daniela e Leticia <treinadev+par04@campuscode.com.br>
2019-09-10 16:49:34 -0300

    API gets customers - tests ready

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::CustomersController < Api::V1::ApiController
     @customers = Customer.all
 
     if @customers.any?
-      render json: @customers, status: 202
+      render json: {customers: @customers}, except: %i[created_at updated_at], status: 202, include: {profile: {except: %i[created_at updated_at]}}
     else
       render json: {message: "Não encontrado"}, status: 404
     end
--- spec/requests/api_gets_customers_spec.rb
+++ spec/requests/api_gets_customers_spec.rb
@@ -23,4 +23,16 @@ describe 'API gets customers' do
     expect(response.status).to eq 404
     expect(response.body).to include 'Não encontrado'
   end
+
+  it 'and chooses which fields to get' do
+    customer1 = create(:customer, email: 'customer1@email.com')
+    profile1 = create(:profile, account: customer1)   
+
+    get api_v1_customers_path
+    json_customers = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to eq 202
+    expect(json_customers[:customers][0][:name]).to include customer1.name
+    expect(json_customers[:customers][0][:profile][:nickname]).to include customer1.profile.nickname
+    expect(json_customers[:customers][0]).not_to include customer1.created_at
+  end
 end
