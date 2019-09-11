10341481c94542705ecd04df8ac9aa7a571637a3
Marcelo/Lucas <treinadev+par09@campuscode.com.br>
2019-09-11 13:12:22 -0300

    Customer View Personal List
    Co-authored-by: Marcelo Z <marcelo.zaninin@outlook.com>
    Co-authored-by: Daniela <dani.adamatti@gmail.com>

--- spec/features/customer_reserves_appointment_spec.rb
+++ spec/features/customer_reserves_appointment_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'Customer reserves an appointment' do
+feature 'Customer view personal list' do
   scenario 'view personals for unit' do
     unit = create(:unit)
     personal = create(:personal, name: 'Personal 1')
@@ -18,23 +18,4 @@ feature 'Customer reserves an appointment' do
     expect(page).not_to have_content('Personal 3')
   end
 
-  scenario 'view personal schedule' do
-    #Arrange
-    unit = create(:unit)
-    personal = create(:personal, name: 'Personal 1')
-    schedule = create(:schedule, personal: personal, unit: unit)   
-    user = create(:customer, unit: unit)
-
-    #Act
-    login_as(user, scope: :account)
-    visit root_path
-    click_on 'Agendar horário com Personal'
-    click_on 'Personal 1'
-
-    #Assert
-    expect(page).to have_content('Personal 1')
-    expect(page).to have_content('Início')
-    expect(page).to have_content('Término')
-
-  end
 end
