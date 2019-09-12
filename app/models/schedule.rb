0d2ce19c849858af82d1dfcf6eb9eade0472ba51
Joao/Lucas <treinadev+par03@campuscode.com.br>
2019-09-12 15:26:10 -0300

    WIP First teste wroten, implementation needed
    Co-authored-by: João <j.rafaeel@hotmail.com>
    Co-authored-by: grun00 <lucas.c.grunheidt@gmail.com>

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,6 @@
 class Schedule < ApplicationRecord
+
+  
   belongs_to :unit
   belongs_to :personal, foreign_key: 'account_id'
 
@@ -6,10 +8,10 @@ class Schedule < ApplicationRecord
 
   validates :date, :start, :finish, :price, :unit_id, :account_id, presence: true
   validates :price, numericality: { greater_than: 0 }
-
+  
+  
   def create_appointments
-    (self.start..self.finish).each do |x| 
-
+    (self.start...self.finish).each do |x| 
       appm = Appointment.create(date_appointment: self.date, start_hour: x, end_hour: x+1, available: false, account: self.personal, schedule: self)
       CustomerAppointment.create(appointment: appm, name_class: "Aulas com Personal")
     end
