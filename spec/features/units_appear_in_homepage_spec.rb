6539dc5ad04a287329d3beb5a5271e3cd5391745
grun00 <lucas.c.grunheidt@gmail.com>
2019-09-14 16:21:30 -0300

    WIP First test writen
    Co-authored-by: grun00 <lucas.c.grunheidt@gmail.com>
    Co-authored-by: maugravena <maugravena@gmail.com>

--- spec/features/units_appear_in_homepage_spec.rb
+++ spec/features/units_appear_in_homepage_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 feature 'Units appear in homepage'do
+  scenario 'Visitor visits home page and see all units' do
+    unit = create(:unit)
+    other_unit = create(:unit)
 
+    visit root_path
+
+    expect(page).to have_content(unit.name) 
+    expect(page).to have_content(other_unit.name)
+  end 
 end
