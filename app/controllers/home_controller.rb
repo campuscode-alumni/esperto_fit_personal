cc24d090212922a84569c7dccbd4cac13a11df23
grun00 <lucas.c.grunheidt@gmail.com>
2019-09-14 16:42:47 -0300

    Error message if no units.
    Co-authored-by: grun00 <lucas.c.grunheidt@gmail.com>
    Co-authored-by: maugravena <maugravena@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,8 @@ class HomeController < ApplicationController
       return redirect_to new_profile_path unless current_account.profile?     
     end
     @units = Unit.all
+    if @units.empty? 
+      flash[:alert] = 'Nenhuma unidade cadastrada'
+    end
   end
 end
