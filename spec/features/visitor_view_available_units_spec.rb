1646badc689b4eaf048cc61cbb09554a786d10ca
Joao/Lucas <treinadev+par03@campuscode.com.br>
2019-09-16 13:55:53 -0300

    WIP changed tests to have more information

--- spec/features/visitor_view_available_units_spec.rb
+++ spec/features/visitor_view_available_units_spec.rb
@@ -12,10 +12,33 @@ feature 'visitor view units' do
 
     #expect(page).to have_text 'Unidade'
 
+    json_response = JSON.parse(
+      File.read(Rails.root.join('spec', 'support', "gyms.json")
+    ), symbolize_names: true)
+
+    
     expect(page).to have_css('.gym', count: 3)
 
     expect(page).to have_css('h3', text: 'Academia 01')
+    expect(page).to have_css('li', text: "Horário de atendimento das #{json_response[0][:open_hour]}\
+                            às #{json_response[:open_hour]} de #{json_response[0][:working_days]}")
+
+    expect(page).to have_css('li', text: "#{json_response[0][:address]}")
+    expect(page).to have_css('li', text: "#{json_response[0][:images][1]}")          
+    
     expect(page).to have_css('h3', text: 'Academia 02')
+    expect(page).to have_css('li', text: "Horário de atendimento das #{json_response[1][:open_hour]}\
+                            às #{json_response[1][:open_hour]} de #{json_response[1][:working_days]}")
+
+    expect(page).to have_css('li', text: "#{json_response[1][:address]}")
+    expect(page).to have_css('li', text: "#{json_response[1][:images][0]}")  
+
     expect(page).to have_css('h3', text: 'Academia 03')
+    expect(page).to have_css('li', text: "Horário de atendimento das #{json_response[2][:open_hour]}\
+                            às #{json_response[2][:open_hour]} de #{json_response[2][:working_days]}")
+
+    expect(page).to have_css('li', text: "#{json_response[2][:address]}")
+    expect(page).to have_css('li', text: "#{json_response[2][:images][0]}")  
   end
+
 end
