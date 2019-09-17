bddec18ade07aa5239bb8906bd773fd5df4e2670
Joao Barbieri <j.rafaeel@hotmail.com>
2019-09-17 00:43:07 -0300

    Reserve appointment completed

--- spec/features/customer_reserves_appointment_spec.rb
+++ spec/features/customer_reserves_appointment_spec.rb
@@ -46,6 +46,7 @@ feature 'Customer can reserve an appointment' do
     #Assert
     expect(page).to have_content('Aula Indisponivel')
     expect(page).to have_link('Escolher essa aula')
+    
 
   end
 
