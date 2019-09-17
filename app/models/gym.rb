db048d211245156daf2fbd28abbddd0969df6c03
grun00 <lucas.c.grunheidt@gmail.com>
2019-09-17 19:17:29 -0300

    Visitor can see units without being loged in

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -12,9 +12,17 @@ class Gym
     response = EspertoAcademy.client.get do |req|
       req.url 'gyms'
     end
-    #byebug
     if response.status == 200 
       return response.body
     end
+  end
+
+  def self.find(id)
+    response = EspertoAcademy.client.get do |req|
+      req.url "gyms/#{id}"
+    end
+    if response.status == 200 
+      return response.body
+    end 
   end
 end
--- spec/support/request_stub.rb
+++ spec/support/request_stub.rb
@@ -8,4 +8,13 @@ module RequestStub
     stub_request(:get, url).
       to_return(status: 200, body: json_response)
   end
+  def get_gym
+    filename = 'gyms.json'
+    url      = "http://0.0.0.0:4000/api/v1/gyms/1"
+    json_response = JSON.parse(
+      File.read(Rails.root.join('spec', 'support', "#{filename}")
+    ), symbolize_names: true)
+    stub_request(:get, url).
+      to_return(status: 200, body: json_response)
+  end
 end
