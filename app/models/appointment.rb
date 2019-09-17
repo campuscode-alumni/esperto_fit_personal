5af8632480d302e64b567776bbba277f8d9d95eb
grun00 <lucas.c.grunheidt@gmail.com>
2019-09-16 21:01:09 -0300

    WIP added has_owner? function to appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,8 @@ class Appointment < ApplicationRecord
   belongs_to :account
   belongs_to :schedule
   has_one :customer, through: :customer_appointment
+
+  def has_owner?
+    CustomerAppointment.find(self.id).account_id
+  end
 end
