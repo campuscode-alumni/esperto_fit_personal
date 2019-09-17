433914fcb442672ba9c2974195a4d41d7c5b4453
grun00 <lucas.c.grunheidt@gmail.com>
2019-09-17 20:13:59 -0300

    Visitor views units test fixed

--- spec/features/visitor_view_available_units_spec.rb
+++ spec/features/visitor_view_available_units_spec.rb
@@ -6,28 +6,21 @@ feature 'visitor view units' do
   end
 
   scenario 'sucessfully' do
+
+    json_response = Gym.all
     visit root_path
 
-    json_response = JSON.parse(
-      File.read(Rails.root.join('spec', 'support', "gyms.json")
-    ), symbolize_names: true)
-
-    expect(page).to have_css('.gym', count: 3)
-
     expect(page).to have_css('h3', text: 'Academia 01')
-    expect(page).to have_css("img[src*='#{json_response[0][:images][0]}']")
-    expect(page).to have_css('li', text: "Horário de atendimento das #{json_response[0][:open_hour]} às #{json_response[0][:close_hour]} de #{json_response[0][:working_days]}")
-    expect(page).to have_css('li', text: "#{json_response[0][:address]}")
+    expect(page).to have_css('p', text: "Funcionamento: de #{json_response[0][:working_days]}, das #{json_response[0][:open_hour]} às #{json_response[0][:close_hour]}")
+    expect(page).to have_css('p', text: "#{json_response[0][:address]}")
 
     expect(page).to have_css('h3', text: 'Academia 02')
-    expect(page).to have_css("img[src*='#{json_response[1][:images][0]}']")
-    expect(page).to have_css('li', text: "Horário de atendimento das #{json_response[1][:open_hour]} às #{json_response[1][:close_hour]} de #{json_response[1][:working_days]}")
-    expect(page).to have_css('li', text: "#{json_response[1][:address]}")
+    expect(page).to have_css('p', text: "Funcionamento: de #{json_response[1][:working_days]}, das #{json_response[1][:open_hour]} às #{json_response[1][:close_hour]}")
+    expect(page).to have_css('p', text: "#{json_response[1][:address]}")
 
     expect(page).to have_css('h3', text: 'Academia 03')
-    expect(page).to have_css("img[src*='#{json_response[2][:images][0]}']")
-    expect(page).to have_css('li', text: "Horário de atendimento das #{json_response[2][:open_hour]} às #{json_response[2][:close_hour]} de #{json_response[2][:working_days]}")
-    expect(page).to have_css('li', text: "#{json_response[2][:address]}")
+    expect(page).to have_css('p', text: "Funcionamento: de #{json_response[2][:working_days]}, das #{json_response[2][:open_hour]} às #{json_response[2][:close_hour]}")
+    expect(page).to have_css('p', text: "#{json_response[2][:address]}")
   end
 
 end
