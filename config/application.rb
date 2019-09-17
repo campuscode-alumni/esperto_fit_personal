b3bafa07b1c5725b23d8030ee7767a04340702a0
Joao Barbieri <j.rafaeel@hotmail.com>
2019-09-17 18:22:57 -0300

    WIP Units

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,7 @@ module EspertoFitPersonal
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.cookbook = config_for(:espertofit).symbolize_keys
   end
 end
---
+++ spec/support/request_stub.rb
@@ -0,0 +1,11 @@
+module RequestStub
+  def list_gyms
+    filename = 'gyms.json'
+    url      = 'http://0.0.0.0:3000/api/v1/gyms'
+    json_response = JSON.parse(
+      File.read(Rails.root.join('spec', 'support', "#{filename}")
+    ), symbolize_names: true)
+    stub_request(:get, url).
+      to_return(status: 200, body: json_response)
+  end
+end
