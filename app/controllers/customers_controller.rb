ce2916598b083d3e42422dec3f91020e2216497f
grun00 <lucas.c.grunheidt@gmail.com>
2019-09-17 18:17:14 -0300

    WIP User can register in Unit from homepage. WIP Correct all tests

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,9 +5,15 @@ class CustomersController < ApplicationController
     if current_account.type == "Customer"
       customer = Customer.find(current_account.id) 
       unit = JSON.parse((Faraday.get "http://localhost:4000/api/v1/gyms/#{params[:id]}").body, symbolize_names: true)
-      customer.profile.update(enrollment_id: unit[:gym][:id])
-      flash[:notice] = 'Matricula realizada com sucesso'
-      redirect_to root_path
+      if customer.profile.enrollment_id == nil 
+        customer.profile.update(enrollment_id: unit[:gym][:id])
+        flash[:notice] = 'Matricula realizada com sucesso'
+        redirect_to root_path
+      else
+        customer.profile.update(enrollment_id: unit[:gym][:id])
+        flash[:notice] = 'Unidade Trocada com sucesso'
+        redirect_to root_path 
+      end
     else
       redirect_to root_path
     end
