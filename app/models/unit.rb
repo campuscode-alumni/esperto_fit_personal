3e0445dbfc8874d3a58519911b7bca1a69bde0ca
Joao/Lucas <treinadev+par03@campuscode.com.br>
2019-09-18 13:52:02 -0300

    WIP Unit has ref to gym API

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -6,4 +6,14 @@ class Unit < ApplicationRecord
   has_many :schedules
   has_many :personals, through: :schedules,
   class_name: 'Account', foreign_key: 'account_id'
+
+  validates :ex_gym_ref, uniqueness: true
+
+
+  def self.load_api
+    byebug
+    Gym.all.each do |gym|
+      Unit.create(name: gym.name, ex_gym_ref: gym.id)
+    end
+  end
 end
