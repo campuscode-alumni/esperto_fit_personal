8079e75aa75194a4d98611de25b43e23748ad9f2
Leticia Mayumi <hashizumemay@gmail.com>
2019-09-18 16:29:30 -0300

    Tests fixed for i18n

--- spec/features/customer_can_see_appointments_spec.rb
+++ spec/features/customer_can_see_appointments_spec.rb
@@ -20,7 +20,7 @@ feature 'Customer can see apppointments' do
     click_on "Personals na #{unit.name}"
     click_on 'Patricia'
 
-    expect(page).to have_css('h1', text: "Agenda da #{profile.first_name} para a data #{schedule.date}")
+    expect(page).to have_css('h1', text: "Agenda de #{profile.first_name} para 10/09/2019")
     expect(page).to have_css('li', text: "Horário das #{(schedule.appointments[0].start_hour)} às #{(schedule.appointments[0].end_hour)} - Preço #{(schedule.price)}")
     expect(page).to have_css('li', text: "Horário das #{(schedule.appointments[1].start_hour)} às #{(schedule.appointments[1].end_hour)} - Preço #{(schedule.price)}")
   end
