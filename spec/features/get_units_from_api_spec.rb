c38d9218f7e43a4f945bb06c942a8f7ddc701d6d
grun00 <lucas.c.grunheidt@gmail.com>
2019-09-18 09:53:54 -0300

    WIP

--- spec/features/get_units_from_api_spec.rb
+++ spec/features/get_units_from_api_spec.rb
@@ -2,11 +2,7 @@ require 'rails_helper'
 
 feature 'Get units from the gym API' do
   scenario 'Successfully'do
-    visit root_path
-    resp = Gym.all 
-    expect(page).to have_content(resp[0][:name])
-    expect(page).to have_content(resp[1][:name])
-    expect(page).to have_content(resp[1][:name])
+    byebug
   end
 
 end
