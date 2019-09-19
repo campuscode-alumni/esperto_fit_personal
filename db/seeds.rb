f2488f5b242c561d29f70d2e233a8d3a89935362
grun00 <lucas.c.grunheidt@gmail.com>
2019-09-18 21:13:02 -0300

    Updated seeds, WIP Fix tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-unit = Unit.create(name: 'Santana')
-Unit.create(name: 'Paulista')
-
+Unit.load_api
+unit = Unit.first
 customer_acc = Account.create(document: '12345678900', email: 'customer@email.com', password: '123456', type: 'Customer', unit: unit)
-personal_acc = Account.create(document: '12345678910', email: 'personal@email.com', password: '123456', type: 'Personal', unit: unit)
+personal_acc = Account.create!(document: '12345678910', email: 'personal@email.com', password: '123456', type: 'Personal', unit: unit)
+
+Enrollment.new(account: customer_acc, unit: unit)
 
 customer_prof = Profile.create(account: customer_acc, first_name: 'Customer', last_name: 'Smith', address: 'Customer Street', date_of_birth: '01-01-1970', contact: '123-456-789', gender: 'male', nickname: 'CustomerNick', payment_method: 'Credito')
 personal_prof = Profile.create(account: personal_acc, first_name: 'Personal', last_name: 'Smith', work_document: '123.456.789.10', address: 'Personal Street', date_of_birth: '01-01-1970', contact: '123-456-789', gender: 'male', nickname: 'PersonalNick', payment_method: 'Credito')
