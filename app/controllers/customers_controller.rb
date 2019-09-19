de12bef90597e889bb3a318a6801aa5fcbc066c7
grun00 <lucas.c.grunheidt@gmail.com>
2019-09-18 22:14:51 -0300

    Increased coverage

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,14 +2,11 @@ class CustomersController < ApplicationController
   before_action :authenticate_account!, only: %i[ add_unit ]
 
   def add_unit
-    if current_account.type == "Customer"
+    redirect_to root_path unless current_account.type == "Customer"
       customer = Customer.find(current_account.id)
       unit = Unit.find(params[:id])
       customer.update(unit: unit)
       flash[:notice] = 'Matricula realizada com sucesso'
       redirect_to root_path
-    else
-      redirect_to root_path
-    end
   end
 end
